44e5bb59412c566564ee17e697d7c9ac3cf0d87d
ayesharizwann <ayesha.rixwan@gmail.com>
2022-11-25 01:07:34 +0500

    backend structure initiated

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+// const express=require('express')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
